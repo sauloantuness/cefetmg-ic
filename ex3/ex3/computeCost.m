8d9ba40fe61fee5a7467e8e28499bb2f46fa5f0b
Saulo Antunes <sauloantuness@gmail.com>
2017-09-24 20:07:14 -0300

    Compute cost exercise

--- ex3/ex3/computeCost.m
+++ ex3/ex3/computeCost.m
@@ -13,9 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+m = length(y);
+S = X * theta - y;
+J = S' * S / (2 * m);
 
 % =========================================================================
 
